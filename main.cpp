3b49126f9b5c9627be10d8f16aef7ea2ee3e6eca
Spider-has <romanh998@gmail.com>
2025-10-25 13:53:18 +0300

    completed practice task

--- main.cpp
+++ main.cpp
@@ -1,9 +1,30 @@
 #include <iostream>
+#include <limits>
+#include <stdexcept>
+
+unsigned safe_multiply(unsigned a, unsigned b) {
+  using lim_int = std::numeric_limits<unsigned>;
+  const unsigned maxUnsigned = lim_int::max();
+  if (a > 0 && b > 0 && a > maxUnsigned / b)
+    throw std::overflow_error("multiply");
+  return a * b;
+}
+
+unsigned safe_add(unsigned a, unsigned b) {
+  using lim_int = std::numeric_limits<unsigned>;
+  const unsigned maxUnsigned = lim_int::max();
+  if (a > maxUnsigned - b)
+    throw std::overflow_error("add");
+  return a + b;
+}
 
 bool isPyth(unsigned a, unsigned b, unsigned c) {
-  bool p = (a * a == b * b + c * c);
-  p = p || (b * b == a * a + c * c);
-  p = p || (c * c == b * b + c * c);
+  bool p = (safe_multiply(a, a) ==
+            safe_add(safe_multiply(b, b), safe_multiply(c, c)));
+  p = p || (safe_multiply(b, b) ==
+            safe_add(safe_multiply(a, a), safe_multiply(c, c)));
+  p = p || (safe_multiply(c, c) ==
+            safe_add(safe_multiply(a, a), safe_multiply(b, b)));
   return p;
 }
 
@@ -12,18 +33,25 @@ int main() {
 
   u_t a = 0, b = 0, c = 0;
   std::cin >> c >> b;
+
   size_t count = 0;
+
   while (std::cin >> a) {
-    count += isPyth(a, b, c) ? 1 : 0;
-    c = b;
-    b = a;
+    try {
+      count += isPyth(a, b, c) ? 1 : 0;
+      c = b;
+      b = a;
+    } catch (const std::overflow_error &e) {
+      std::cerr << "Overflow: " << e.what() << "\n";
+      return 2;
+    }
   }
 
   if (std::cin.eof()) {
     std::cout << count;
     std::cout << "\n";
   } else if (std::cin.fail()) {
-    std::cerr << "Error\n";
+    std::cerr << "Error input reading\n";
     return 1;
   }
 }
